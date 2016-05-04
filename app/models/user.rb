4d74442f1e31ab8047b058686f63dc130d57125d
文月文月吃八盤(?) <requiem.for.memories@gmail.com>
2016-05-04 07:08:00 +0000

    add photo url

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,14 @@ class User < ActiveRecord::Base
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
+      user.photo = auth.info.image
     end
   end
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.github_data"] && session["devise.github_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
+        
       end
     end
   end     
