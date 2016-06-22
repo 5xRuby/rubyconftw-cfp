8f5d4ddb8662b06639038840b43107e0524bbcee
文月文月吃八盤(?) <requiem.for.memories@gmail.com>
2016-06-22 03:56:49 +0000

    Add check permission function

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,3 +1,12 @@
 class Admin::ApplicationController < ApplicationController
+  before_action :check_permission
   
+  private
+  
+  def check_permission
+    unless user_signed_in? && current_user.is_admin
+    redirect_to root_path
+    flash[:warning] = "Permission Denied"
+    end
+  end
 end
