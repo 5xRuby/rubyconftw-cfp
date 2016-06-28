73f197bcfea5ef1d332f6bed3f2b2d3e0495935c
Turkey <turkey060535@gmail.com>
2016-06-28 17:01:39 +0800

    My proposal 列出使用者投稿了哪些活動，每個活動投了幾個稿件

--- app/controllers/users/index_controller.rb
+++ app/controllers/users/index_controller.rb
@@ -1,7 +1,10 @@
 class Users::IndexController < ApplicationController
   def show
-    # @submits = current_user.papers
-    @submits = current_user.papers
-
+  	@activities = Activity.all
+  	# @activities.each do |activity|
+  	# 	activity.papers.where(user_id: current_user.id ).each do |paper|
+  	# end
+    # @submits = current_user.papers.where(activity_id: params[:activity_id])
+                                                                                                                  
   end
 end
