4d62102b01c82153c5de5e2e89a99e36ab0738de
文月 <requiem.for.memories@gmail.com>
2016-07-26 04:05:54 +0000

    新增後台的papers頁面

--- config/routes.rb
+++ config/routes.rb
@@ -4,25 +4,26 @@ Rails.application.routes.draw do
   resources :activities, only: [:index,:show] do
     resources :papers
     resources :categories
-    get '/reviews' => 'reviews#index', as: 'reviews' 
+    get '/reviews' => 'reviews#index', as: 'reviews'
     get '/reviews/:id' => 'reviews#review', as: 'review'
     get '/reviews/:id/reviewed' => 'reviews#reviewed', as: 'reviewed'
   end
-  
+
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   as :user do
     get 'users' => "users/index#show"
     get 'users/edit' => 'users/registrations#edit', :as => 'edit_user_registration'
     put 'users' => 'users/registrations#update', :as => 'user_registration'
   end
-  
+
   namespace :admin do
     resources :activities do
       resources :reviewers, only: [:index,:new,:create,:destroy]
+      resources :papers, only: [:index, :show]
     end
     get '/users' => "users#index"
     get '/users/:id/designate' => "users#designate", as: 'user_designate'
     get '/users/:id/undesignate' => "users#undesignate", as: 'user_undesignate'
   end
-    
+
 end
