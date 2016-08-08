3f85fbf7fb4db9897e3c0298f31f7c459b71614d
蒼時弦也 <elct9620@frost.tw>
2016-08-08 14:22:35 +0800

    Add unreview paper count on navigation

--- app/helpers/admin/activities_helper.rb
+++ app/helpers/admin/activities_helper.rb
@@ -1,2 +1,9 @@
 module Admin::ActivitiesHelper
+  def admin_review_paper_link(activity)
+    label = "Review Papers"
+    unreviewed_papers = activity.papers.size - activity.review_by(current_user).size
+    label << "(#{unreviewed_papers})"
+
+    link_to label, admin_activity_papers_path(activity)
+  end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,6 +4,7 @@ class Activity < ApplicationRecord
 	has_many :user_activity_relationships, dependent: :destroy
   has_many :users, through: :user_activity_relationships
   has_many :custom_fields, lambda {order("sort_order")}, dependent: :destroy
+  has_many :reviews, through: :papers
   accepts_nested_attributes_for :custom_fields, allow_destroy: true
   mount_uploader :logo, LogoUploader
 
@@ -14,5 +15,9 @@ class Activity < ApplicationRecord
   end
   def open?
     activity.end_date > Time.now
+  end
+
+  def review_by(user)
+    reviews.where(user: user)
   end
 end
