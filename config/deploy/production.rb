d2277a555122c88beda7ad42e48628c721cc2b8a
Awaru Ryudo <ryudoawaru@gmail.com>
2016-08-08 15:18:29 +0800

    Fix production deploy script location 	modified:   config/deploy/production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-set :deploy_to, '/home/rubytw/staging-apps/cfp-stg.rubyconf.tw'
+set :deploy_to, '/home/rubytw/rubyconf.tw/cfp'
 role :app, %w{rubytw@cfp.rubyconf.tw:14159}
 role :web, %w{rubytw@cfp.rubyconf.tw:14159}
 role :db,  %w{rubytw@cfp.rubyconf.tw:14159}
