57b6bda8b92e321962e6d19a5c76aafb94895e93
蒼時弦也 <elct9620@frost.tw>
2016-08-08 16:55:33 +0800

    Add markdown renderer for paper review notice mail

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,7 +3,15 @@ class NotificationMailer < ApplicationMailer
 
   def notice(email, subject, content)
     # TODO: Apply markdown to show rich content
-    @content = content
+    @content = markdown(content)
     mail to: email, subject: subject
+  end
+
+  protected
+  def markdown(content)
+    renderer = Redcarpet::Render::HTML.new(filter_html: true)
+    markdown = Redcarpet::Markdown.new(renderer, autolink: true)
+
+    markdown.render(content)
   end
 end
