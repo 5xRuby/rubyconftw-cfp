a4ed125a9b703cb769b87da68dd26d9d8a7f8826
蒼時弦也 <elct9620@frost.tw>
2016-08-09 14:57:25 +0800

    Fix no activity cannot show correct unreviewed count

--- app/helpers/admin/activities_helper.rb
+++ app/helpers/admin/activities_helper.rb
@@ -1,7 +1,7 @@
 module Admin::ActivitiesHelper
   def admin_review_paper_link(activity)
-    return 0 if activity.nil?
     label = "Review Papers"
+    return label if activity.nil?
     unreviewed_papers = activity.papers.size - activity.review_by(current_user).size
     label << "(#{unreviewed_papers})"
 
