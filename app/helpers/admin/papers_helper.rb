2f5cb44d5c8774e8e36382e6443b99cf204437a2
文月 <requiem.for.memories@gmail.com>
2016-08-09 09:09:48 +0000

    修正審搞狀況被蓋掉的問題

--- app/helpers/admin/papers_helper.rb
+++ app/helpers/admin/papers_helper.rb
@@ -1,16 +1,16 @@
 module Admin::PapersHelper
   def admin_review_button(paper)
-    return if paper.reviewed_by?(current_user)
+    return if !paper.may_view?
     link_to "Review", admin_paper_reviews_path(paper), method: "POST", class: "btn btn-success"
   end
 
   def admin_accept_button(paper)
-    return if !paper.reviewed?
+    return if !paper.may_accept?
       link_to "Accept", admin_paper_accept_path(paper), method: "POST", class: "btn btn-success"
   end
 
   def admin_reject_button(paper)
-    return if !paper.reviewed?
+    return if !paper.may_reject?
       link_to "Reject", admin_paper_reject_path(paper), method: "POST", class: "btn btn-danger"
   end
 end
