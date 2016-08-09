9ba9fdfa839efba0bb0bd7f2f62f439937ec1366
蒼時弦也 <elct9620@frost.tw>
2016-08-09 14:47:02 +0800

    Fix nil activity case unreviewed count broken

--- app/helpers/admin/activities_helper.rb
+++ app/helpers/admin/activities_helper.rb
@@ -1,5 +1,6 @@
 module Admin::ActivitiesHelper
   def admin_review_paper_link(activity)
+    return 0 if activity.nil?
     label = "Review Papers"
     unreviewed_papers = activity.papers.size - activity.review_by(current_user).size
     label << "(#{unreviewed_papers})"
