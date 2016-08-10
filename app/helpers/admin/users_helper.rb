f618b6b55cb6ca98745156c21d558fd2c9c3ff16
蒼時弦也 <elct9620@frost.tw>
2016-08-10 10:19:43 +0800

    Adjust user admin ui to use bootstrap style

--- app/helpers/admin/users_helper.rb
+++ app/helpers/admin/users_helper.rb
@@ -1,2 +1,9 @@
 module Admin::UsersHelper
+  def toggle_admin_permission_button(user)
+    if user.is_admin?
+      link_to "undesignate to be admin ", admin_user_undesignate_path(user), class: "btn btn-danger"
+    else
+      link_to "designate to be admin ", admin_user_designate_path(user), class: "btn btn-danger"
+    end
+  end
 end
