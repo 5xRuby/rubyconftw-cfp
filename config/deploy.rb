a3f4159b676d0fc97136d4820f013f09ac4390ac
蒼時弦也 <elct9620@frost.tw>
2016-08-10 18:03:13 +0800

    Close capistrano pty to prevent sidekiq bug

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :format, :pretty
 set :log_level, :debug
 
 # Default value for :pty is false
-set :pty, true
+set :pty, false
 
 # Default value for :linked_files is []
 set :linked_files, %w{config/database.yml config/application.yml config/sidekiq.yml}
