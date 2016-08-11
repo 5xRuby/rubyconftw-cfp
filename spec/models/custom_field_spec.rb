95db2428f8c7e80ab82054b906a4e63265177e3d
蒼時弦也 <elct9620@frost.tw>
2016-08-11 18:35:49 +0800

    Add custom field test

--- spec/models/custom_field_spec.rb
+++ spec/models/custom_field_spec.rb
@@ -6,4 +6,19 @@ RSpec.describe CustomField, type: :model do
   it { should validate_presence_of(:sort_order) }
   it { should validate_presence_of(:field_type) }
 
+  context "include collection" do
+    let(:collection) { ["A", "B", "C"] }
+
+    it "should able to convert collection into string" do
+      custom_field = CustomField.new(collection: collection)
+      expect(custom_field.collection_text).to eq(collection.join(CustomField::DELIMITER))
+    end
+
+    it "should able to convert string into collection" do
+      custom_field = CustomField.new
+      custom_field.collection_text = collection.join(CustomField::DELIMITER)
+      expect(custom_field.collection).to eq(collection)
+    end
+  end
+
 end
