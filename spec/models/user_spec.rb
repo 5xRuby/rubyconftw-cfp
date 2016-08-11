0a3a7d20298d5d31578cd154ffc859ecb0e26964
蒼時弦也 <elct9620@frost.tw>
2016-08-11 16:23:58 +0800

    Add omniauth relate test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,40 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it { should validate_presence_of(:name) }
+
+  context "sign in with omniauth" do
+    let(:exists_user) { FactoryGirl.create(:user) }
+    let(:auth_info) {
+      OmniAuth::AuthHash.new({
+        provider: 'github',
+        info: {
+          email: 'github@5xruby.tw'
+        }
+      })
+    }
+
+    let(:auth_info_exists) {
+      OmniAuth::AuthHash.new({
+        provider: 'github',
+        info: {
+          email: exists_user.email
+        }
+      })
+    }
+
+    it "should create new user when not found" do
+      user_count = User.count
+      User.from_omniauth(auth_info)
+      expect(user_count).not_to eq(User.count)
+    end
+
+    it "should return exists user when found" do
+      expect(User.from_omniauth(auth_info_exists)).to eq(exists_user)
+    end
+  end
+
+  context "with default information from session" do
+    it "should copy github information"
+    it "should copy twitter information"
+  end
 end
