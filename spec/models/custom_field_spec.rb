1ff95c18af0c1112af70903271d48a92c3d7906c
蒼時弦也 <elct9620@frost.tw>
2016-08-11 14:37:43 +0800

    Add custom field validation test

--- spec/models/custom_field_spec.rb
+++ spec/models/custom_field_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe CustomField, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:sort_order) }
+  it { should validate_presence_of(:field_type) }
+
 end
