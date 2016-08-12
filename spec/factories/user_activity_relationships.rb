5dd3ab37fadb9d11dfe9dc8474de7d381cbb2364
蒼時弦也 <elct9620@frost.tw>
2016-08-12 10:37:40 +0800

    Add ability test with other model

---
+++ spec/factories/user_activity_relationships.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :user_activity_relationship do
+    
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,7 @@
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 require 'aasm/rspec'
+require "cancan/matchers"
 require 'simplecov'
 SimpleCov.start 'rails'
 
