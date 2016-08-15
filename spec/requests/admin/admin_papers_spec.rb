933f496ecf53636a5939b26189514547d62b3d35
蒼時弦也 <elct9620@frost.tw>
2016-08-15 12:21:35 +0800

    Clean up admin/papers to use before(:each)

--- spec/requests/admin/admin_papers_spec.rb
+++ spec/requests/admin/admin_papers_spec.rb
@@ -3,9 +3,19 @@ require 'rails_helper'
 RSpec.describe "Admin::Papers", type: :request do
   let(:admin) { FactoryGirl.create(:user, :admin) }
   let(:activity) { FactoryGirl.create(:activity) }
-  let(:paper) { FactoryGirl.create(:paper, activity: activity) }
+  let(:state) { :submitted }
+
+  before(:each) do
+    @paper = FactoryGirl.create(:paper, activity: activity, state: state)
+    login_as admin
+  end
 
   describe "GET /admin/activity/:id/papers" do
+    before(:each) do
+      @papers = FactoryGirl.create_list(:paper, 5, activity: activity)
+      visit admin_activity_papers_url(activity)
+    end
+
     it "cannot viewed by non admin user" do
       user = FactoryGirl.create(:user)
       login_as user
@@ -14,10 +24,7 @@ RSpec.describe "Admin::Papers", type: :request do
     end
 
     it "display specify activity's proposal" do
-      papers = FactoryGirl.create_list(:paper, 5, activity: activity)
-      login_as admin
-      visit admin_activity_papers_url(activity)
-      papers.each do |paper|
+      @papers.each do |paper|
         expect(page).to have_content(paper.title)
       end
     end
@@ -25,29 +32,25 @@ RSpec.describe "Admin::Papers", type: :request do
 
   describe "GET /admin/activity/:id/papers/:id" do
 
-    it "display paper's detail information" do
-      login_as admin
-      visit admin_activity_paper_url(activity, paper)
+    before(:each) { visit admin_activity_paper_url(activity, @paper) }
 
-      expect(page).to have_content(paper.title)
-      expect(page).to have_content(paper.abstract)
-      expect(page).to have_content(paper.outline)
-      expect(page).to have_content(paper.pitch)
+    it "display paper's detail information" do
+      expect(page).to have_content(@paper.title)
+      expect(page).to have_content(@paper.abstract)
+      expect(page).to have_content(@paper.outline)
+      expect(page).to have_content(@paper.pitch)
     end
   end
 
   describe "POST /admin/papers/:id/reviews" do
+    before(:each) { visit admin_activity_papers_url(activity) }
+
     it "change paper state to reviewed" do
-      paper = FactoryGirl.create(:paper, activity: activity)
-
-      login_as admin
-      visit admin_activity_papers_url(activity)
-
-      within "#paper_#{paper.id}" do
+      within "#paper_#{@paper.id}" do
         click_link "Review"
       end
 
-      within "#paper_#{paper.id}" do
+      within "#paper_#{@paper.id}" do
         expect(page).to have_content("Accept")
         expect(page).to have_content("Reject")
         expect(page).not_to have_content("Review")
@@ -56,17 +59,15 @@ RSpec.describe "Admin::Papers", type: :request do
   end
 
   describe "POST /admin/papers/:id/accept" do
+    let(:state) { :reviewed }
+    before(:each) { visit admin_activity_papers_url(activity) }
+
     it "change paper state to accepted" do
-      paper = FactoryGirl.create(:paper, :reviewed, activity: activity)
-
-      login_as admin
-      visit admin_activity_papers_url(activity)
-
-      within "#paper_#{paper.id}" do
+      within "#paper_#{@paper.id}" do
         click_link "Accept"
       end
 
-      within "#paper_#{paper.id}" do
+      within "#paper_#{@paper.id}" do
         expect(page).not_to have_content("Accept")
         expect(page).not_to have_content("Reject")
         expect(page).not_to have_content("Review")
@@ -75,17 +76,15 @@ RSpec.describe "Admin::Papers", type: :request do
   end
 
   describe "POST /admin/papers/:id/reject" do
+    let(:state) { :reviewed }
+    before(:each) { visit admin_activity_papers_url(activity) }
+
     it "change paper state to rejected" do
-      paper = FactoryGirl.create(:paper, :reviewed, activity: activity)
-
-      login_as admin
-      visit admin_activity_papers_url(activity)
-
-      within "#paper_#{paper.id}" do
+      within "#paper_#{@paper.id}" do
         click_link "Reject"
       end
 
-      within "#paper_#{paper.id}" do
+      within "#paper_#{@paper.id}" do
         expect(page).not_to have_content("Accept")
         expect(page).not_to have_content("Reject")
         expect(page).not_to have_content("Review")
