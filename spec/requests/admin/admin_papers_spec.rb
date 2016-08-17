ead32ea543509228b0bcabf780e3e866b5dafe54
文月 <requiem.for.memories@gmail.com>
2016-08-17 03:06:55 +0000

    補上comment的測試

--- spec/requests/admin/admin_papers_spec.rb
+++ spec/requests/admin/admin_papers_spec.rb
@@ -106,31 +106,69 @@ RSpec.describe "Admin::Papers", type: :request do
     end
 
     it "render form after clicking button" do
-      find("#paper_#{@paper.id} .edit-tag-button").click
-      #within "#paper_#{@paper.id}" do
-      #  find('.add-tag-field').click
-      #  #click_link "edit tag"
-      #end
+      #find("#paper_#{@paper.id} .edit-tag-button").click
+      #find("#paper_#{@paper.id} .add-tag-field").click
       #expect(page).to have_css(".edit-tag-button")
     end
 
     it "update tags" do
-
-      #click_link "edit tag"
+      #find("#paper_#{@paper.id} .edit-tag-button").click
       #within ".add-tag-field" do
       #  fill_in "Tag list", with: "abc, foo"
       #  click_button "Save"
       #end
+      #expect(page).to have_content("abc")
+      #expect(page).to have_content("foo")
     end
 
     it "do not render second form while clicking button" do
+      #2.times{ find("#paper_#{@paper.id} .edit-tag-button").click }
+      #...
     end
 
     it "put tab_list into values if tags already exist" do
+      #...
     end
 
     it "remove form when clicking close button" do
+      #...
+    end
+  end
+
+  describe "comments" do
+    before(:each) do
+      @paper = FactoryGirl.create(:paper, :reviewed, activity: activity)
+      login_as admin
+      visit admin_activity_paper_url(activity, @paper)
     end
 
+    it "create new comments" do
+      within "form#new_comment" do
+        fill_in "comment[text]", with: "Hello ruby!"
+        click_on "Submit"
+      end
+      expect(page).to have_content("#{admin.name} says:")
+      expect(page).to have_content("Hello ruby!")
+      expect(page).to have_content("delete")
+    end
+
+    it "delete user's comments" do
+      comment = FactoryGirl.create(:comment, paper: @paper, user:admin, text: "Hello rails!")
+      visit admin_activity_paper_url(activity, @paper)
+      within ".comment-#{comment.id}" do
+        click_on "delete"
+      end
+       expect(page).not_to have_content("Hello rails!")
+    end
+
+    it "cannot delete other user's comment" do
+      other_admin = FactoryGirl.create(:user, :admin)
+      comment = FactoryGirl.create(:comment, paper: @paper, user: other_admin, text: "Hello Rspec!")
+      visit admin_activity_paper_url(activity, @paper)
+      expect(page).to have_content("Hello Rspec!")
+      expect(page).not_to have_content("delete")
+    end
   end
+
+
 end
