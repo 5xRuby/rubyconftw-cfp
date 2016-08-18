f49cdb91ca159330f6de60959e23bf838fe95065
文月 <requiem.for.memories@gmail.com>
2016-08-18 09:42:37 +0000

    新增permalink

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,6 +4,11 @@ class ActivitiesController < ApplicationController
 	end
 
 	def show
-		@activity = Activity.find(params[:id])
+    if params[:id] =~ /\A\d+\z/
+      @activity = Activity.find(params[:id])
+      redirect_to activity_path(@activity)
+    else
+		  @activity = Activity.find_by(permalink: params[:id])
+    end
 	end
 end
--- app/controllers/admin/papers_controller.rb
+++ app/controllers/admin/papers_controller.rb
@@ -24,7 +24,7 @@ class Admin::PapersController < Admin::ApplicationController
 
 
   def set_activity
-    @activity = Activity.find(params[:activity_id])
+    @activity = Activity.find(permalink: params[:activity_id])
   end
 
   def set_paper
