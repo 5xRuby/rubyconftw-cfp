f258783183e230baf87bac768ca88e186255f826
蒼時弦也 <elct9620@frost.tw>
2016-10-17 10:58:55 +0800

    Add stats filter to check all papers and only speaker papers

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -2,8 +2,26 @@ class Admin::StatsController < ApplicationController
   before_action -> { @activity = Activity.find_by(permalink: params[:activity_id]) }
 
   def show
-    @total_papers = @activity.papers.size
+    @total_papers = papers.size
     @custom_fields = @activity.custom_fields
-    @custom_field_answers = @activity.custom_field_answers
+    @custom_field_answers = custom_field_answers
+  end
+
+  def papers
+    return @activity.papers.where(user_id: speakers) if speaker_only?
+    @activity.papers
+  end
+
+  def custom_field_answers
+    return @activity.custom_field_answers.where(user_id: speakers) if speaker_only?
+    @activity.custom_field_answers
+  end
+
+  def speakers
+    @activity.speakers
+  end
+
+  def speaker_only?
+    params[:speaker_only]
   end
 end
--- app/helpers/admin/stats_helper.rb
+++ app/helpers/admin/stats_helper.rb
@@ -1,5 +1,6 @@
 module Admin::StatsHelper
   def draw_chart(type, datas, options = {})
+    return if datas.empty?
     chart_label = datas.keys
     chart_series = datas.values
     content_tag :div, nil, class: "ct-chart ct-perfect-fourth", data: { type: type, labels: chart_label, series: chart_series, options: options}
@@ -11,5 +12,10 @@ module Admin::StatsHelper
     when "radios", "checkboxes" then render "pie_chart", field: field, stats: datas.stats(papers)
     else render "data_table", field: field, datas: datas.includes(:user)
     end
+  end
+
+  def toggle_speaker_stats_button
+    return link_to "Show All", {}, class: "btn btn-success" if params[:speaker_only]
+    link_to "Show Sepaker", {speaker_only: true}, class: "btn btn-primary"
   end
 end
