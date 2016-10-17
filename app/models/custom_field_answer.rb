79732d1ffba0acb27daa5e112fa9c94833398bb7
蒼時弦也 <elct9620@frost.tw>
2016-10-17 12:31:27 +0800

    Add custom field answers test

--- app/models/custom_field_answer.rb
+++ app/models/custom_field_answer.rb
@@ -3,8 +3,9 @@ class CustomFieldAnswer < ApplicationRecord
   belongs_to :user
   belongs_to :custom_field
 
-  def self.stats(base_count = 0)
+  def self.stats(base_count = nil)
     stats = group(:answer).size
+    return stats if base_count.nil?
     unknowns = stats.values.sum - base_count
     return stats unless unknowns != 0
     stats["Unknown"] = unknowns.abs
--- spec/models/custom_field_answer_spec.rb
+++ spec/models/custom_field_answer_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe CustomFieldAnswer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:activity) { FactoryGirl.create(:activity) }
+  let!(:custom_fields) { FactoryGirl.create_list(:custom_field, 5, :checkboxes, activity: activity) }
+  let!(:papers) { FactoryGirl.create_list(:paper, 5, activity: activity, answer_of_custom_fields: answer_of_custom_fields) }
+
+  let(:answer_of_custom_fields) do
+    answer = {}
+    custom_fields.each do |field|
+      answer[field.id.to_s] = "Yes"
+    end
+    answer
+  end
+
+  before(:each) do
+    @field = activity.custom_fields.first
+    @answers = activity.custom_field_answers
+  end
+
+  it "return stats data" do
+    expect(@answers.where(custom_field: @field).stats).to eq({"Yes" => papers.count})
+  end
+
+  it "return stats data with base" do
+    expect(@answers.where(custom_field: @field).stats(10)).to eq({"Yes" => papers.count, "Unknown" => 10 - papers.count})
+  end
 end
