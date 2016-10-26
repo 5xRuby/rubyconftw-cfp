b46326f2e413ec8ac17aaec25c7bb82441e3aa27
Liang-Chi Tseng <lctseng@cs.nctu.edu.tw>
2016-10-26 18:07:27 +0800

    add data validation (do not blank) at service info

--- spec/mailers/previews/notifier_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/notifier_mailer
-class NotifierMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/notifier_mailer/send_message
-  def send_message
-    NotifierMailerMailer.send_message
-  end
-
-end
