f53e5e9209a9b9d1cc83ba0b3a69ec2186458b9d
Awaru Ryudo <ryudoawaru@gmail.com>
2017-09-08 12:38:22 +0800

    Add new deploy settings

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,5 @@
-set :deploy_to, '/home/rubytw/rubyconf.tw/cfp'
-role :app, %w{rubytw@cfp.rubyconf.tw:14159}
-role :web, %w{rubytw@cfp.rubyconf.tw:14159}
-role :db,  %w{rubytw@cfp.rubyconf.tw:14159}
+set :default_env, { path: "$PATH:/usr/local/ruby-2.4.1/bin:" }
+set :deploy_to, '/home/deploy/rubyconf-cfp'
+role :app, %w{deploy@10.128.128.154}
+role :web, %w{deploy@10.128.128.154}
+role :db,  %w{deploy@10.128.128.154}
