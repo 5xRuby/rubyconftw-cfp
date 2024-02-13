50293abc413097e5992ae08329187b306d45896d
ryudo <ryudo@5xruby.com>
2024-02-13 04:00:55 +0000

    Upgrade 5.2 and remove Capistrano, Sidekiq

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,8 @@ Bundler.require(*Rails.groups)
 
 module CfpSystem
   class Application < Rails::Application
+    config.load_defaults 5.2
     config.assets.quiet = true
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-
     config.time_zone = "Asia/Taipei"
     config.action_view.field_error_proc = Proc.new { |html_tag, instance|
       if instance.error_message.kind_of?(Array)
@@ -24,8 +20,5 @@ module CfpSystem
       html_tag <<  %(<span class="help-block">#{error_message}</span>).html_safe
       %(<div class="form-group has-error">#{html_tag}</div>).html_safe
     }
-
-    config.active_job.queue_adapter = :sidekiq
-    config.time_zone = 'Asia/Taipei'
   end
 end
--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -2,4 +2,7 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
-Mime::Type.register 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', :xlsx
+Mime::Type.register 'application/yml', :yml
+Mime::Type.register 'application/pdf', :pdf
+Mime::Type.register "application/xls", :xls
+Mime::Type.register "application/xlsx", :xlsx
